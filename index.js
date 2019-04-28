1a62fdfa9cb38d4439b078aa5278968172924366
briantam23 <briantam23@gmail.com>
2019-04-28 07:13:43 -0400

    Added getIn() examples

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const { Map, List, fromJS } = require('immutable');
 
+/* // Convert JS to Immutable
+
 // Normal Javascript
 const person = {
   name: 'Will',
   pets: ['cat', 'dog']
 };
 // To create the equivalent in Immutable:
-/* const immutablePerson = Map({
+const immutablePerson = Map({
   name: 'Will',
   pets: List(['cat', 'dog'])
-}); */
+});
 // Or ...
-const immutablePerson = fromJS(person);
+const immutablePerson = fromJS(person); */
 
-console.log(immutablePerson)
+/* // Convert back from Immutable to JS
+
+const immutablePerson = Map({ name: 'Will' });
+const person = immutablePerson.toJS();
+
+console.log(person); // prints { name: 'Will' }; */
+
+
+// Getting a nested value from an object without checking if it exists
+
+/* // Normal Javascript
+const data = { my: { nested: { name: 'Brian' } } };
+
+const goodName = data.my.nested.name;
+console.log(goodName); // prints Brian
+
+const badName = data.my.bad.name; 
+console.log(badName); // throws error: 'Cannot read name of undefined' */
+
+// To create the equivalent in Immutable:
+const data = fromJS({ my: { nested: { name: 'Will' } } });
+
+const goodName = data.getIn(['my', 'nested', 'name']);
+console.log(goodName); // prints Will
+
+const badName = data.getIn(['my', 'lovely', 'name']);
+console.log(badName); // prints undefined - no error thrown
