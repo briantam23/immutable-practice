205b4b7b1ec9f5aeb470cd84ab518dc1e156a9e0
briantam23 <briantam23@gmail.com>
2019-04-28 07:17:18 -0400

    Added Chaining Manipulations examples

--- index.js
+++ index.js
@@ -34,11 +34,28 @@ console.log(goodName); // prints Brian
 const badName = data.my.bad.name; 
 console.log(badName); // throws error: 'Cannot read name of undefined' */
 
-// To create the equivalent in Immutable:
+/* // To create the equivalent in Immutable:
 const data = fromJS({ my: { nested: { name: 'Will' } } });
 
 const goodName = data.getIn(['my', 'nested', 'name']);
 console.log(goodName); // prints Will
 
 const badName = data.getIn(['my', 'lovely', 'name']);
-console.log(badName); // prints undefined - no error thrown
+console.log(badName); // prints undefined - no error thrown */
+
+// Chaining Manipulations
+
+/* // Normal Javascript
+const pets = ['cat', 'dog'];
+
+pets.push('goldfish');
+pets.push('tortoise');
+
+console.log(pets); // prints ['cat', 'dog', 'goldfish', 'tortoise']; */
+
+// To create the equivalent in Immutable:
+const pets = List(['cat', 'dog']);
+const finalPets = pets.push('goldfish').push('tortoise');
+
+console.log(pets.toJS()); // prints ['cat', 'dog'];
+console.log(finalPets.toJS()); // prints ['cat', 'dog', 'goldfish', 'tortoise'];
